0329675a58e64d2dc964529f967a742e57c28c84
elnaz-24 <elnaz.khani24@gmail.com>
2024-11-23 21:14:03 +0300

    Add Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
+import "./input.css";
 
 function App() {
-  return <div>Quiz app</div>;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Quiz APP With Tailwind CSS!</h1>
+    </div>
+  );
 }
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", // مسیر فایل‌های React
+    "./public/index.html"        // فایل HTML اصلی
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
