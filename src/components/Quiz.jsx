5d1d07d6b3eac5824bb8ccfc28dd479681987a0f
elnaz-24 <elnaz.khani24@gmail.com>
2024-11-26 13:11:27 +0300

    Highlighting Selected Answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,9 +4,12 @@ import quizCompleteImage from "../assets/quiz-complete.png";
 import QuestionTimer from "../QuestionTimer";
 
 export default function Quiz() {
+  const [answerState, setAnswerState] = useState("");
   // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
+
+  const activeQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
   // //shuffle
   // const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
   // // shuffledAnswers.sort((a,b)=>1)
@@ -14,15 +17,33 @@ export default function Quiz() {
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer=useCallback( function handleSelectAnswer(selectedAnswer) {
-    setUserAnswers((prevUserAnswers) => {
-      return [...prevUserAnswers, selectedAnswer];
-    });
-  },[])
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      //Highlighting Selected Answers
+      setAnswerState("answered");
 
-// progress bar
-const handleSkipAnswer=useCallback(() => handleSelectAnswer(null),[handleSelectAnswer])
+      setUserAnswers((prevUserAnswers) => {
+        return [...prevUserAnswers, selectedAnswer];
+      });
+      setTimeout(() => {
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
 
+  // progress bar
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   //quiz complete
   if (quizIsComplete) {
@@ -42,22 +63,40 @@ const handleSkipAnswer=useCallback(() => handleSelectAnswer(null),[handleSelectA
     <div id="quiz">
       <div id="question">
         <QuestionTimer
-        key={activeQuestionIndex}
+          key={activeQuestionIndex}
           timeout={10000}
           // onTimeout={() => handleSelectAnswer(null)}
           onTimeout={handleSkipAnswer}
-
         />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {/* {QUESTIONS[activeQuestionIndex].answers.map((answer) => ( */}
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
+
+          {shuffledAnswers.map((answer) => {
+            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+            let cssClass = "";
+
+            if (answerState === "answered" && isSelected) {
+              cssClass = "selected";
+            }
+            if (
+              (answerState === "correct" || answerState === "wrong") &&
+              isSelected
+            ) {
+              cssClass = answerState;
+            }
+
+            return (
+              <li key={answer} className="answer">
+                <button
+                  onClick={() => handleSelectAnswer(answer)}
+                  className={cssClass}
+                >
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
